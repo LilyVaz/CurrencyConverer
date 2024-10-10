989a114ef125a92b5c0787554ceaeb70e813a06d
LilyVaz <vasquezlily75@gmail.com>
2024-10-10 14:57:58 -0600

    Convertidor De Moneda

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
     public static void main(String[]args){
-        Scanner scanner= new Scanner(System.in);
-        try {
-            System.out.println("Ingrese la mondeda que desea convertir");
-            String baseCurrency = scanner.nextLine().toUpperCase();
-            System.out.println("Ingrese la moneda de destino");
-            String targetCurrency = scanner.nextLine().toUpperCase();
-            System.out.println("Ingree el monto a convertir");
-            double cantidad=scanner.nextDouble();
+        try (Scanner scanner = new Scanner(System.in)) {
+            try {
+                System.out.println("Ingrese la mondeda que desea convertir");
+                String baseCurrency = scanner.nextLine().toUpperCase();
+                System.out.println("Ingrese la moneda de destino");
+                String targetCurrency = scanner.nextLine().toUpperCase();
+                System.out.println("Ingree el monto a convertir");
+                double cantidad = scanner.nextDouble();
 
-            ConsultasTasaCambio consultasTasaCambio=new ConsultasTasaCambio();
-            TasaCambio tasaCambio= consultasTasaCambio.obtenerTasaCambio(baseCurrency, targetCurrency);
-            ConvertidorDeMoneda convertidorDeMoneda=new ConvertidorDeMoneda();
-            double montoConvertido=convertidorDeMoneda.convertir(cantidad, tasaCambio);
-            System.out.printf("El monto convertido de %s a %s es: %.2f $s\n",baseCurrency, targetCurrency,montoConvertido, targetCurrency);
-        } catch (Exception e) {
-            System.out.println("Eror: "+e.getMessage());
-        }finally {
-            scanner.close();
+                ConsultasTasaCambio consultasTasaCambio = new ConsultasTasaCambio();
+                TasaCambio tasaCambio = consultasTasaCambio.obtenerTasaCambio(baseCurrency, targetCurrency);
+                ConvertidorDeMoneda convertidorDeMoneda = new ConvertidorDeMoneda();
+                double montoConvertido = convertidorDeMoneda.convertir(cantidad, tasaCambio);
+                System.out.printf("El monto convertido de %s a %s es: %.2f %s\n", baseCurrency, targetCurrency, montoConvertido, targetCurrency);
+            } catch (Exception e) {
+                System.out.println("Eror: " + e.getMessage());
+            } finally {
+                scanner.close();
+            }
         }
     }
 }
